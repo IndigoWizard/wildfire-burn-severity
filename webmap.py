b3355e2d5528472c9f94eb78e1e045b7bbfe0690
IndigoWizard <microclubist@gmail.com>
2022-09-24 19:01:24 +0100

    GEE config
    setting up google earth engine configuration and drawing method with folium

--- webmap.py
+++ webmap.py
@@ -1,5 +1,30 @@
+import ee
+from ee import image
 import folium
 import webbrowser
+
+#################### Earth Engine Configuration #################### 
+# ########## Earth Engine Setup
+# Triggering authentification to earth engine services
+# Uncomment then execute only once > auth succecfull > put back as a comment:
+ee.Authenticate()
+
+# initializing the earth engine library
+ee.Initialize()
+
+# ##### earth-engine drawing method setup
+def add_ee_layer(self, ee_image_object, vis_params, name):
+  map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
+  folium.raster_layers.TileLayer(
+      tiles = map_id_dict['tile_fetcher'].url_format,
+      attr = 'Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
+      name = name,
+      overlay = True,
+      control = True
+  ).add_to(self)
+
+# configuring earth engine display rendering method in folium
+folium.Map.add_ee_layer = add_ee_layer
 
 #################### MAIN MAP ####################
 m = folium.Map(location = [36.606519, 2.372596], tiles='OpenStreetMap', zoom_start = 12, control_scale = True)
